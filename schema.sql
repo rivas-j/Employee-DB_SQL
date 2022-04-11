a119dd7d785f12e1a5d30aa48b4a121c82df730a
rivas-j <jrivas404@gmail.com>
2022-04-10 21:41:49 -0400

    Updated SQL Schema

--- schema.sql
+++ schema.sql
@@ -34,3 +34,24 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE managers (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),	
+  PRIMARY KEY (dept_no, emp_no)	
+);
+
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR(40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),	
+  PRIMARY KEY (emp_no, from_date)
+);
+
